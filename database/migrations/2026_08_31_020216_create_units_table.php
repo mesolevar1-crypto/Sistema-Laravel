a57e070be0da40ef38e28150a66ae49fc76c5864
mesolevar1-crypto <mesolevar1@gmail.com>
2026-09-01 08:37:33 -0500

    Tablas ya completamente emigradas y listas

--- database/migrations/2026_08_31_020216_create_units_table.php
+++ database/migrations/2026_08_31_020216_create_units_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('units', function (Blueprint $table) {
             $table->id('id_unidad');
             $table->string('nombre');
+            $table->timestamps();
         });
     }
 
